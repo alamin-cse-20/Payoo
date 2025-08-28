239a765ff1775758f366f2237a0a720626e172e2
alamin-cse-20 <alamin.sust2020@gmail.com>
2025-08-28 20:00:04 +0600

    implemented add money feature

---
+++ script/home.js
@@ -0,0 +1,30 @@
+// console.log('home.js file is connected... babe!');
+
+const validPin = 1234;
+
+
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (e) {
+    e.preventDefault();
+    const bank = document.getElementById('bank').value;
+    const accountNumber = document.getElementById('account-number').value;
+    const amount = parseInt(document.getElementById('add-amount').value);
+    const pin = parseInt(document.getElementById('add-pin').value);
+    // console.log(bank, accountNumber, amount, pin);;
+    const availableBalance = document.getElementById('available-balance').innerText;
+    // console.log(availableBalance);
+
+    if (accountNumber.length !== 11) {
+      alert('Account number must be 11 digit');
+      return;
+    }
+    if (pin !== validPin) {
+      alert('Invalid pin');
+      return;
+    }
+
+    const newAvailableBalance = amount + parseInt(availableBalance);
+    document.getElementById('available-balance').innerText =newAvailableBalance;
+    
+  });
