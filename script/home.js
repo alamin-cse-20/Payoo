9da7a961e461a847572d12b0d303e21503ee9c41
alamin-cse-20 <alamin.sust2020@gmail.com>
2025-08-28 20:55:22 +0600

    toggling add money and cash out

--- script/home.js
+++ script/home.js
@@ -28,3 +28,14 @@ document
     document.getElementById('available-balance').innerText =newAvailableBalance;
     
   });
+
+  // toggling feature
+  document.getElementById('add-button').addEventListener('click', function(){
+    document.getElementById('cash-out-parent').style.display = 'none';
+    document.getElementById('add-money-parent').style.display = 'block';
+  })
+  document.getElementById('cash-out-button').addEventListener('click', function(){
+    document.getElementById('add-money-parent').style.display = 'none';
+    document.getElementById('cash-out-parent').style.display = 'block';
+    
+  })
